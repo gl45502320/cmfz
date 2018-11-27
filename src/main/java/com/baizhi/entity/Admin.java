d059b98e4d1b06e09b308bd199c9953cbda00acb
Your Name <1395219618@qq.com>
2018-11-27 15:23:38 +0800

    v1

---
+++ src/main/java/com/baizhi/entity/Admin.java
@@ -0,0 +1,15 @@
+package com.baizhi.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin {
+    private int id;
+    private String name;
+    private String password;
+}
